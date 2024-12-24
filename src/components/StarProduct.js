7bf4aa0f573ba8ea551bde3a85b31c725f923379
Pearl331 <aqsa212104@gmail.com>
2024-12-24 12:22:11 +0500

    Update StarProduct.js

--- src/components/StarProduct.js
+++ src/components/StarProduct.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import "../styles/StarProduct.css"
+import React from 'react';
+import "../styles/StarProduct.css";
 
-const StarProduct = ({starProduct}) => {
+const StarProduct = ({ starProduct }) => {
   return (
-    
-      <div className="starProduct">
-      <div> <a href={starProduct[0].url}> <img src={starProduct[0].image} alt="1st Product" /></a></div>
-      <div>
-      <a href={starProduct[1].url}> <img src={starProduct[1].image} alt="1st Product" /></a>
-         <a href={starProduct[2].url}> <img src={starProduct[2].image} alt="1st Product" /></a>
-         <a href={starProduct[3].url}> <img src={starProduct[3].image} alt="1st Product" /></a>
+    <div className="starProduct">
+      <div className="main-image">
+        <a href={starProduct[0].url}>
+          <img src={starProduct[0].image} alt="1st Product" />
+        </a>
+      </div>
+      <div className="other-products">
+        {starProduct.slice(1).map((product, index) => (
+          <a key={index} href={product.url}>
+            <img src={product.image} alt={`Product ${index + 2}`} />
+          </a>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StarProduct
+export default StarProduct;
