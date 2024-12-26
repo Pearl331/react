3a1b77d6a754a305e1296242a0d6fc37c49f7a53
Pearl331 <aqsa212104@gmail.com>
2024-12-26 19:32:39 +0500

    Update ProductReviews.js

--- src/components/ProductReviews.js
+++ src/components/ProductReviews.js
@@ -2,19 +2,21 @@ import React from 'react'
 import "../styles/ProductReviews.css"
 import ProductReviewCard from "./ProductReviewCard.js"
 
-const ProductReviews = ({productReviews}) => {
+const ProductReviews = ({ productReviews }) => {
     return (
         <div className="ProductReviews">
-           
-        {productReviews.map((item,index)=>(
-
-<ProductReviewCard price={item.price} name={item.name} image={item.image} review={item.review} index={index} key={item.image} />
-
-
-        ))}
-             
+            {productReviews.map((item, index) => (
+                <ProductReviewCard 
+                    key={item.image}
+                    price={item.price} 
+                    name={item.name} 
+                    image={item.image} 
+                    review={item.review} 
+                    index={index} 
+                />
+            ))}
         </div>
     )
 }
 
-export default ProductReviews
+export default ProductReviews;
