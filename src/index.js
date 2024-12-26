196c50924d32322190fba2a9b4a0810809cf0015
Pearl331 <aqsa212104@gmail.com>
2024-12-26 11:49:29 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
+// Create a root element and render the App component inside it
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendering the App component with StrictMode enabled for development checks
 root.render(
   <React.StrictMode>
     <App />
-
   </React.StrictMode>
 );
